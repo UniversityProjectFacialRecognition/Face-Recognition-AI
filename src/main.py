2f658fcdc4ec1fc8318d1d2d8eb6c0c1e3e153ef
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-09-05 16:06:34 -0300

    feat: Added code to access the webcam

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print("To work!")
+import cv2
+
+# https://docs.opencv.org/4.x/
+
+def run() -> None:
+	video_cap = cv2.VideoCapture(0)
+
+	while(True):
+		frame = video_cap.read()
+
+		cv2.imshow("Your face", frame)
+
+		if(cv2.waitKey(1) == ord('q')):
+			print("Exiting...")
+			break
+
+def main() -> int:
+	print("Identifying Face...")
+
+	run()
+
+if(__name__ == "__main__"):
+	main()
